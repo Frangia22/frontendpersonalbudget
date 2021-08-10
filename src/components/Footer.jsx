165949701dfeb934de20177b2252d8fbf572b3a9
Frangia22 <giacherofranc22@gmail.com>
2021-08-09 22:58:01 -0300

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css'; 
+
+export function Footer() {
+    return (
+        <footer id="my-contact" className="container cf"> 
+            <div className="footer py-2">
+                <p className="card-text">Derechos Reservados 2021 (c)</p>
+            </div>
+        </footer> 
+    )
+}
